b284c7e2e881483112d1c4a4e3167c29e3639d8f
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2025-04-02 20:47:54 +0200

    0.09.1

--- src/data/RepoUsuarioMem.kt
+++ src/data/RepoUsuarioMem.kt
@@ -1,4 +1,37 @@
 package data
 
-class RepoUsuarioMem {
+import model.Perfil
+import model.Usuario
+
+class RepoUsuarioMem: IRepoUsuarios {
+
+    val usuarios = mutableListOf<Usuario>()
+
+    override fun agregar(usuario: Usuario): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun buscar(nombreUsuario: String): Usuario? {
+        TODO("Not yet implemented")
+    }
+
+    override fun eliminar(usuario: Usuario): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun eliminar(nombreUsuario: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun obtenerTodos(): List<Usuario> {
+        TODO("Not yet implemented")
+    }
+
+    override fun obtener(perfil: Perfil): List<Usuario> {
+        TODO("Not yet implemented")
+    }
+
+    override fun cambiarClave(usuario: Usuario, nuevaClave: String): Boolean {
+        TODO("Not yet implemented")
+    }
 }
